d096f9b5d72358ff5f7f405982f8637b52c6719c
ChaeyeonSon <thscodus97@gmail.com>
2020-04-27 16:32:03 +0900

    finish?

--- main.py
+++ main.py
@@ -49,130 +49,130 @@ def main():
     os.environ['CUDA_DEVICE_ORDER']="PCI_BUS_ID"
     os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.device_num)
     tf.config.set_soft_device_placement(True)
-    model = SRGenerator(device=FLAGS.device, device_num=FLAGS.device_num) 
+    with tf.device("/device:"+FLAGS.device+":"+str(FLAGS.device_num)):
+        model = SRGenerator() 
 
-    if FLAGS.is_train:
-        train_dataset = make_dataset(FLAGS.train_img_dir, FLAGS.train_label_dir, train=True,batch_size=FLAGS.batch_size)
-        train_it = train_dataset.make_initializable_iterator()
+        if FLAGS.is_train:
+            train_dataset = make_dataset(FLAGS.train_img_dir, FLAGS.train_label_dir, train=True,batch_size=FLAGS.batch_size)
+            train_it = train_dataset.make_initializable_iterator()
 
-        valid_dataset = make_dataset(FLAGS.valid_img_dir, FLAGS.valid_label_dir, train=False, batch_size=1) #dataset을 10개로 자르자
-        valid_it = valid_dataset.make_initializable_iterator()
-        
-        train_x, train_y = train_it.get_next()
-        valid_x, valid_y = valid_it.get_next()
-
-        train_pred = model.forward(train_x, True)
-        train_loss = model.loss_function(train_y, train_pred)
-        train_op = model.optimize(train_loss)
-
-        valid_pred = model.forward(valid_x, False)
-        valid_loss = model.loss_function(valid_y, valid_pred) 
-        var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
-        saver = tf.train.Saver(var_list)
-        with tf.name_scope('train_summary'):
-            ts1= tf.summary.image('input_summary', train_x)
-            ts2 = tf.summary.image('target_summary', train_y)
-            ts3 = tf.summary.image('outputs_summary', train_pred)
-        train_img_merged = tf.summary.merge([ts1, ts2, ts3])
-        with tf.name_scope('valid_summary'):
-            vs1= tf.summary.image('input_summary', valid_x)
-            vs2 = tf.summary.image('target_summary', valid_y)
-            vs3 = tf.summary.image('outputs_summary', valid_pred)
-        valid_img_merged = tf.summary.merge([vs1, vs2, vs3])
-
-        train_loss_avg = tf.placeholder(tf.float32)
-        valid_loss_avg = tf.placeholder(tf.float32)
-        ts_loss = tf.summary.scalar('train_loss', train_loss_avg)
-        vs_loss = tf.summary.scalar('valid_loss', valid_loss_avg) 
-        scalar_merged= tf.summary.merge([ts_loss,vs_loss])
-        
-
-        with tf.Session() as sess:
-
-            writer = tf.summary.FileWriter('./board/graph', sess.graph)
-            writer.add_graph(sess.graph)
-
-            sess.run(tf.global_variables_initializer())
-            loaded, start_epoch = model.load(sess, saver, FLAGS.checkpoint_dir)
-            if loaded:
-                print(" [*] Load SUCCESS")
-            else:
-                print(" [!] Load failed...")
+            valid_dataset = make_dataset(FLAGS.valid_img_dir, FLAGS.valid_label_dir, train=False, batch_size=1) #dataset을 10개로 자르자
+            valid_it = valid_dataset.make_initializable_iterator()
             
-            for epoch in tqdm(range(start_epoch,FLAGS.epochs)):
-                start_time = time.time()
-                print("HI")
-                sess.run(train_it.initializer)
-                t_loss = 0.0
-                count = 0
-                try:
+            train_x, train_y = train_it.get_next()
+            valid_x, valid_y = valid_it.get_next()
+
+            train_pred = model.forward(train_x, True)
+            train_loss = model.loss_function(train_y, train_pred)
+            train_op = model.optimize(train_loss)
+
+            valid_pred = model.forward(valid_x, False)
+            valid_loss = model.loss_function(valid_y, valid_pred) 
+            var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
+            saver = tf.train.Saver(var_list)
+            with tf.name_scope('train_summary'):
+                ts1= tf.summary.image('input_summary', train_x)
+                ts2 = tf.summary.image('target_summary', train_y)
+                ts3 = tf.summary.image('outputs_summary', train_pred)
+            train_img_merged = tf.summary.merge([ts1, ts2, ts3])
+            with tf.name_scope('valid_summary'):
+                vs1= tf.summary.image('input_summary', valid_x)
+                vs2 = tf.summary.image('target_summary', valid_y)
+                vs3 = tf.summary.image('outputs_summary', valid_pred)
+            valid_img_merged = tf.summary.merge([vs1, vs2, vs3])
+
+            train_loss_avg = tf.placeholder(tf.float32)
+            valid_loss_avg = tf.placeholder(tf.float32)
+            ts_loss = tf.summary.scalar('train_loss', train_loss_avg)
+            vs_loss = tf.summary.scalar('valid_loss', valid_loss_avg) 
+            scalar_merged= tf.summary.merge([ts_loss,vs_loss])
+            
+
+            with tf.Session() as sess:
+
+                writer = tf.summary.FileWriter('./board/graph', sess.graph)
+                writer.add_graph(sess.graph)
+
+                sess.run(tf.global_variables_initializer())
+                loaded, start_epoch = model.load(sess, saver, FLAGS.checkpoint_dir)
+                if loaded:
+                    print(" [*] Load SUCCESS")
+                else:
+                    print(" [!] Load failed...")
+                
+                for epoch in tqdm(range(start_epoch,FLAGS.epochs)):
+                    start_time = time.time()
+                    sess.run(train_it.initializer)
+                    t_loss = 0.0
+                    count = 0
+                    try:
+                        while True:
+                            # loss = 0
+                            # _ = sess.run(train_x)
+                            _, loss, train_img_summary = sess.run([train_op, train_loss, train_img_merged])
+                            t_loss += loss
+                            # print("count : %d"%count)
+                            count += 1
+                    except tf.errors.OutOfRangeError:
+                        pass
+                    t_loss /= count
+                    sess.run(valid_it.initializer)
+                    v_loss = 0.0
+                    count = 0
                     while True:
-                        # loss = 0
-                        # _ = sess.run(train_x)
-                        _, loss, train_img_summary = sess.run([train_op, train_loss, train_img_merged])
-                        t_loss += loss
-                        # print("count : %d"%count)
-                        count += 1
-                except tf.errors.OutOfRangeError:
-                    pass
-                t_loss /= count
+                        try:
+                            # _ = sess.run(valid_x)
+                            # v_loss = 0
+                            loss, valid_img_summary = sess.run([valid_loss, valid_img_merged])
+                            v_loss += loss
+                            count += 1 
+                        except tf.errors.OutOfRangeError:
+                            break
+                    v_loss /= count
+                    print("Epoch: [%2d], time: [%4.4f], train_loss: [%.8f], valid_loss: [%.8f]"% ((epoch+1), time.time()-start_time, t_loss, v_loss))
+                    model.save(sess, saver, FLAGS.checkpoint_dir, epoch)
+                    
+                    summary = sess.run(scalar_merged, feed_dict={train_loss_avg: t_loss, valid_loss_avg: v_loss})
+                
+                    writer.add_summary(train_img_summary, epoch)
+                    writer.add_summary(valid_img_summary, epoch)
+                    writer.add_summary(summary, epoch)
+            
+        else:
+            valid_dataset = make_dataset(FLAGS.valid_img_dir, FLAGS.valid_label_dir, train=False, batch_size=1)
+            valid_it = valid_dataset.make_initializable_iterator()
+            valid_pred = model.forward(valid_x, False)
+            valid_loss = model.loss_function(valid_y, valid_pred) 
+            with tf.Session() as sess:
+                if model.load(sess, saver, FLAGS.checkpoint_dir):
+                    print(" [*] Load SUCCESS")
+                else:
+                    print(" [!] Load failed...")
                 sess.run(valid_it.initializer)
                 v_loss = 0.0
                 count = 0
-                while True:
-                    try:
-                        # _ = sess.run(valid_x)
-                        # v_loss = 0
-                        loss, valid_img_summary = sess.run([valid_loss, valid_img_merged])
+                f = open(FLAGS.sample_dir+"/result.txt","w")
+                try:
+                    while True:
+                        start_time = time.time()
+                        valid_psnr = compute_psnr(valid_y, valid_pred)
+                        loss, psnr = sess.run([valid_loss, valid_psnr])
                         v_loss += loss
-                        count += 1 
-                    except tf.errors.OutOfRangeError:
-                        break
+                        if count < 10:
+                            bicubic_x = bicubic_upsample_x2(valid_x)
+                            bicubic_psnr = compute_psnr(valid_y, bicubic_psnr)
+                            bic, pred, bic_psnr = sess.run([bicubic_x, valid_pred, bicubic_psnr])
+                            save_image(FLAGS.sample_dir+"/bicubic_"+str(count)+".jpg", bic)
+                            save_image(FLAGS.sample_dir+"/pred_"+str(count)+".jpg", pred)
+                            f.write("%dth img => time: [%4.4f], loss: [%.8f], psnr: [%.4f], bicubic_psnr: [%.4f]\n"% ((count+1), time.time()-start_time, loss, psnr, bic_psnr))
+                        else:
+                            f.write("%dth img => time: [%4.4f], loss: [%.8f], psnr: [%.4f]\n"% ((count+1), time.time()-start_time, loss, psnr))
+                        count += 1
+                except tf.errors.OutOfRangeError:
+                    pass
                 v_loss /= count
-                print("Epoch: [%2d], time: [%4.4f], train_loss: [%.8f], valid_loss: [%.8f]"% ((epoch+1), time.time()-start_time, t_loss, v_loss))
-                model.save(sess, saver, FLAGS.checkpoint_dir, epoch)
-                
-                summary = sess.run(scalar_merged, feed_dict={train_loss_avg: t_loss, valid_loss_avg: v_loss})
-               
-                writer.add_summary(train_img_summary, epoch)
-                writer.add_summary(valid_img_summary, epoch)
-                writer.add_summary(summary, epoch)
-        
-    else:
-        valid_dataset = make_dataset(FLAGS.valid_img_dir, FLAGS.valid_label_dir, train=False, batch_size=1)
-        valid_it = valid_dataset.make_initializable_iterator()
-        valid_pred = model.forward(valid_x, False)
-        valid_loss = model.loss_function(valid_y, valid_pred) 
-        with tf.Session() as sess:
-            if model.load(sess, saver, FLAGS.checkpoint_dir):
-                print(" [*] Load SUCCESS")
-            else:
-                print(" [!] Load failed...")
-            sess.run(valid_it.initializer)
-            v_loss = 0.0
-            count = 0
-            f = open(FLAGS.sample_dir+"/result.txt","w")
-            try:
-                while True:
-                    start_time = time.time()
-                    valid_psnr = compute_psnr(valid_y, valid_pred)
-                    loss, psnr = sess.run([valid_loss, valid_psnr])
-                    v_loss += loss
-                    if count < 10:
-                        bicubic_x = bicubic_upsample_x2(valid_x)
-                        bicubic_psnr = compute_psnr(valid_y, bicubic_psnr)
-                        bic, pred, bic_psnr = sess.run([bicubic_x, valid_pred, bicubic_psnr])
-                        save_image(FLAGS.sample_dir+"/bicubic_"+str(count)+".jpg", bic)
-                        save_image(FLAGS.sample_dir+"/pred_"+str(count)+".jpg", pred)
-                        f.write("%dth img => time: [%4.4f], loss: [%.8f], psnr: [%.4f], bicubic_psnr: [%.4f]\n"% ((count+1), time.time()-start_time, loss, psnr, bic_psnr))
-                    else:
-                        f.write("%dth img => time: [%4.4f], loss: [%.8f], psnr: [%.4f]\n"% ((count+1), time.time()-start_time, loss, psnr))
-                    count += 1
-            except tf.errors.OutOfRangeError:
-                pass
-            v_loss /= count
-            f.write("Avg. Loss : %.8f"%v_loss)
-            f.close()
+                f.write("Avg. Loss : %.8f"%v_loss)
+                f.close()
 
 if __name__=='__main__':
     main()
--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from data_process import *
 
 
 class SRGenerator:
-  def __init__(self, device='CPU', device_num = 0, content_loss='mse', use_gan=False, learning_rate=1e-4, num_blocks=16, num_upsamples=2):
+  def __init__(self, content_loss='mse', use_gan=False, learning_rate=1e-4, num_blocks=16, num_upsamples=2):
       self.learning_rate = learning_rate
       self.num_blocks = num_blocks
       self.num_upsamples = num_upsamples
@@ -21,7 +21,7 @@ class SRGenerator:
           print('Invalid content loss function. Must be \'mse\', \'vgg22\', or \'vgg54\'.')
           exit()
       self.content_loss = content_loss
-      self.device = "/device:"+device+":"+str(device_num)
+      #self.device = "/device:"+device+":"+str(device_num)
   
   def _residual_block(self, x, kernel_size, filters, strides=1, training=False):
     x = tf.nn.relu(x)
@@ -43,28 +43,27 @@ class SRGenerator:
 
   def forward(self, x, is_train):
     """Builds the forward pass network graph"""
-    with tf.device(self.device):
-      with tf.variable_scope('generator', reuse=tf.AUTO_REUSE) as scope:
-        x = tf.layers.conv2d(x, kernel_size=9, filters=64, strides=1, padding='same')
-        #x = tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
-        skip = x
+    with tf.variable_scope('generator', reuse=tf.AUTO_REUSE) as scope:
+      x = tf.layers.conv2d(x, kernel_size=9, filters=64, strides=1, padding='same')
+      #x = tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
+      skip = x
 
-        # B x ResidualBlocks
-        for i in range(self.num_blocks):
-          with tf.name_scope("ResBlock_"+str(i)):
-            x = self._residual_block(x, kernel_size=3, filters=64, strides=1, training=is_train)
+      # B x ResidualBlocks
+      for i in range(self.num_blocks):
+        with tf.name_scope("ResBlock_"+str(i)):
+          x = self._residual_block(x, kernel_size=3, filters=64, strides=1, training=is_train)
 
-        x = tf.nn.relu(x)
-        x = tf.layers.conv2d(x, kernel_size=3, filters=64, strides=1, padding='same', use_bias=False)
-        x = tf.layers.batch_normalization(x, training=is_train)
-        x = x + skip
+      x = tf.nn.relu(x)
+      x = tf.layers.conv2d(x, kernel_size=3, filters=64, strides=1, padding='same', use_bias=False)
+      x = tf.layers.batch_normalization(x, training=is_train)
+      x = x + skip
 
-        # Upsample blocks
-        for i in range(self.num_upsamples-1):
-          with tf.name_scope("Upsample_"+str(i)):
-            x = self._Upsample2xBlock(x, kernel_size=3, filters=256)
-        
-        x = tf.layers.conv2d(x, kernel_size=9, filters=3, strides=1, padding='same', name='forward')
+      # Upsample blocks
+      for i in range(self.num_upsamples-1):
+        with tf.name_scope("Upsample_"+str(i)):
+          x = self._Upsample2xBlock(x, kernel_size=3, filters=256)
+      
+      x = tf.layers.conv2d(x, kernel_size=9, filters=3, strides=1, padding='same', name='forward')
     return x
   
   def loss_function(self, y, y_pred):
