bc91f54aa3ba6cc4ca861c8cce4160cc656e99eb
ChaeyeonSon <thscodus97@gmail.com>
2020-04-27 12:59:40 +0900

    start

--- main.py
+++ main.py
@@ -21,10 +21,13 @@ flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing inp
 flags.DEFINE_integer("stride", 14, "The size of stride to apply input image [14]")
 flags.DEFINE_string("checkpoint_dir", "./checkpoint", "Name of checkpoint directory [checkpoint]")
 flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
-flags.DEFINE_string("train_img_dir", "../DIV2K_train_LR_bicubic/*.png", "Name of train img directory [train_img]")
-flags.DEFINE_string("train_label_dir", "../DIV2K_train_HR/*.png", "Name of train label directory [train_label]")
-flags.DEFINE_string("valid_img_dir", "../DIV2K_valid_LR_bicubic/*.png", "Name of valid img directory [valid_img]")
-flags.DEFINE_string("valid_label_dir", '../DIV2K_valid_HR/*.png', "Name of svalid label directory [valid_label]")
+flags.DEFINE_string("train_img_dir", "../train_data/DIV2K_train_LR_bicubic/*.png", "Name of train img directory [train_img]")
+flags.DEFINE_string("train_label_dir", "../train_data/DIV2K_train_HR/*.png", "Name of train label directory [train_label]")
+flags.DEFINE_string("valid_img_dir", "../valid_data/DIV2K_valid_LR_bicubic/*.png", "Name of valid img directory [valid_img]")
+flags.DEFINE_string("valid_label_dir", '../valid_data/DIV2K_valid_HR/*.png', "Name of svalid label directory [valid_label]")
+flags.DEFINE_string("test_img_dir", "../test_data/DIV2K_valid_LR_bicubic/*.png", "Name of valid img directory [valid_img]")
+flags.DEFINE_string("test_label_dir", '../test_data/DIV2K_valid_HR/*.png', "Name of svalid label directory [valid_label]")
+
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [True]")
 flags.DEFINE_integer("gpu", 0, "Which GPU to use")
 
@@ -69,21 +72,23 @@ def main():
         valid_loss = model.loss_function(valid_y, valid_pred) 
         var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
         saver = tf.train.Saver(var_list)
-        with tf.name_scope('inputs_summary'):
-            tf.summary.image('input_summary', train_x)
-
-        with tf.name_scope('targets_summary'):
-            tf.summary.image('target_summary', train_y)
-
-        with tf.name_scope('outputs_summary'):
-            tf.summary.image('outputs_summary', train_pred)
+        with tf.name_scope('train_summary'):
+            ts1= tf.summary.image('input_summary', train_x)
+            ts2 = tf.summary.image('target_summary', train_y)
+            ts3 = tf.summary.image('outputs_summary', train_pred)
+        train_img_merged = tf.summary.merge([ts1, ts2, ts3])
+        with tf.name_scope('valid_summary'):
+            vs1= tf.summary.image('input_summary', valid_x)
+            vs2 = tf.summary.image('target_summary', valid_y)
+            vs3 = tf.summary.image('outputs_summary', valid_pred)
+        valid_img_merged = tf.summary.merge([vs1, vs2, vs3])
 
         train_loss_avg = tf.placeholder(tf.float32)
         valid_loss_avg = tf.placeholder(tf.float32)
-        tf.summary.scalar('train_loss', train_loss_avg)
-        tf.summary.scalar('valid_loss', valid_loss_avg) 
-
-        merged = tf.summary.merge_all()
+        ts_loss = tf.summary.scalar('train_loss', train_loss_avg)
+        vs_loss = tf.summary.scalar('valid_loss', valid_loss_avg) 
+        scalar_merged= tf.summary.merge([ts_loss,vs_loss])
+        
 
         with tf.Session() as sess:
 
@@ -105,26 +110,35 @@ def main():
                 count = 0
                 try:
                     while True:
-                        loss = 0
-                        _ = sess.run(train_x)
-                        #_, loss = sess.run([train_op, train_loss])
+                        # loss = 0
+                        # _ = sess.run(train_x)
+                        _, loss, train_img_summary = sess.run([train_op, train_loss, train_img_merged])
                         t_loss += loss
-                        print("count : %d"%count)
+                        # print("count : %d"%count)
                         count += 1
                 except tf.errors.OutOfRangeError:
                     pass
                 t_loss /= count
                 sess.run(valid_it.initializer)
                 v_loss = 0.0
-                for i in range(10):
-                    _ = sess.run(valid_x)
-                    v_loss = 0
-                    #v_loss += sess.run(valid_loss)/10
+                count = 0
+                while True:
+                    try:
+                        # _ = sess.run(valid_x)
+                        # v_loss = 0
+                        loss, valid_img_summary = sess.run([valid_loss, valid_img_merged])
+                        v_loss += loss
+                        count += 1 
+                    except tf.errors.OutOfRangeError:
+                        break
+                v_loss /= count
                 print("Epoch: [%2d], time: [%4.4f], train_loss: [%.8f], valid_loss: [%.8f]"% ((epoch+1), time.time()-start_time, t_loss, v_loss))
                 model.save(sess, saver, FLAGS.checkpoint_dir, epoch)
                 print("where TT")
-                summary = sess.run(merged, feed_dict={train_loss_avg: t_loss, valid_loss_avg: v_loss})
+                summary = sess.run(scalar_merged, feed_dict={train_loss_avg: t_loss, valid_loss_avg: v_loss})
                 print("isyou?")
+                writer.add_summary(train_img_summary, epoch)
+                writer.add_summary(valid_img_summary, epoch)
                 writer.add_summary(summary, epoch)
                 print("TTTT")
         
--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from data_process import *
 
 
 class SRGenerator:
-  def __init__(self, content_loss='mse', use_gan=False, learning_rate=1e-4, num_blocks=16, num_upsamples=2):
+  def __init__(self, gpu=True, device = 0, content_loss='mse', use_gan=False, learning_rate=1e-4, num_blocks=16, num_upsamples=2):
       self.learning_rate = learning_rate
       self.num_blocks = num_blocks
       self.num_upsamples = num_upsamples
@@ -21,6 +21,10 @@ class SRGenerator:
           print('Invalid content loss function. Must be \'mse\', \'vgg22\', or \'vgg54\'.')
           exit()
       self.content_loss = content_loss
+      if gpu:
+        self.device = "/gpu:"+str(device)
+      else:
+        self.device = "/cpu:"+str(device)
       #self.saver = tf.train.Saver()
       #self.sess = None
 
@@ -53,28 +57,29 @@ class SRGenerator:
 
   def forward(self, x, is_train):
     """Builds the forward pass network graph"""
-    with tf.variable_scope('generator', reuse=tf.AUTO_REUSE) as scope:
-      x = tf.layers.conv2d(x, kernel_size=9, filters=64, strides=1, padding='same')
-      #x = tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
-      skip = x
+    with tf.device(self.device):
+      with tf.variable_scope('generator', reuse=tf.AUTO_REUSE) as scope:
+        x = tf.layers.conv2d(x, kernel_size=9, filters=64, strides=1, padding='same')
+        #x = tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
+        skip = x
 
-      # B x ResidualBlocks
-      for i in range(self.num_blocks):
-        with tf.name_scope("ResBlock_"+str(i)):
-          x = self._residual_block(x, kernel_size=3, filters=64, strides=1, training=is_train)
+        # B x ResidualBlocks
+        for i in range(self.num_blocks):
+          with tf.name_scope("ResBlock_"+str(i)):
+            x = self._residual_block(x, kernel_size=3, filters=64, strides=1, training=is_train)
 
-      x = tf.nn.relu(x)
-      x = tf.layers.conv2d(x, kernel_size=3, filters=64, strides=1, padding='same', use_bias=False)
-      x = tf.layers.batch_normalization(x, training=is_train)
-      x = x + skip
+        x = tf.nn.relu(x)
+        x = tf.layers.conv2d(x, kernel_size=3, filters=64, strides=1, padding='same', use_bias=False)
+        x = tf.layers.batch_normalization(x, training=is_train)
+        x = x + skip
 
-      # Upsample blocks
-      for i in range(self.num_upsamples-1):
-        with tf.name_scope("Upsample_"+str(i)):
-          x = self._Upsample2xBlock(x, kernel_size=3, filters=256)
-      
-      x = tf.layers.conv2d(x, kernel_size=9, filters=3, strides=1, padding='same', name='forward')
-      return x
+        # Upsample blocks
+        for i in range(self.num_upsamples-1):
+          with tf.name_scope("Upsample_"+str(i)):
+            x = self._Upsample2xBlock(x, kernel_size=3, filters=256)
+        
+        x = tf.layers.conv2d(x, kernel_size=9, filters=3, strides=1, padding='same', name='forward')
+    return x
   
   def loss_function(self, y, y_pred):
     """Loss function"""
